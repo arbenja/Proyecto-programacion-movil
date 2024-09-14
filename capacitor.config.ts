e0178229a2f72e153e98428b3ffe71602b52af70
arbenja <133251497+arbenja@users.noreply.github.com>
2024-09-13 22:15:01 -0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'proyecto.ionic',
+  appName: 'proyecto-ionic',
+  webDir: 'www'
+};
+
+export default config;
